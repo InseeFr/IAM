0e34d6ec5e8e415abfea75eb15f977dd8f3465c6
Emmanuel Demey <demey.emmanuel@gmail.com>
2020-02-19 09:40:33 +0100

    feat: first commit

---
+++ src/build-dictionary.js
@@ -0,0 +1,34 @@
+import {
+	createDictionary,
+	firstLang,
+	secondLang,
+	getLang,
+} from '@inseefr/wilco';
+
+const dictionary = {
+	authorizationTitle: {
+		fr: 'Habilitations',
+		en: 'Authorizations',
+	},
+	pickedRolePlaceholder: {
+		fr: 'Sélectionner un rôle...',
+		en: 'Select a role...',
+	},
+	nameTitle: {
+		fr: 'Nom',
+		en: 'Name',
+	},
+	stampTitle: {
+		fr: 'Timbre',
+		en: 'Stamp',
+	},
+	roleTitle: {
+		fr: 'Rôle',
+		en: 'Role',
+	},
+};
+
+export const D1 = createDictionary(firstLang, dictionary);
+export const D2 = createDictionary(secondLang, dictionary);
+
+export default getLang() === firstLang ? D1 : D2;

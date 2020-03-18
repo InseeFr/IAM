e530f6238f628f8d2b96cb39a762a827e1cc0fdb
travis <>
2020-03-18 08:10:00 +0000

    Rebuild doc for revision aa91e62fca5f7b7c1c6145ce7a36966dcfb9bef4: Deploy example

---
+++ README.md
@@ -0,0 +1,3 @@
+# IAM example
+
+A simple example, showing how IAM can be used, is deployed [online](https://inseefr.github.io/IAM/example).
---
+++ example/precache-manifest.08954e1d3b2f214ce758429a7e2eeb05.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860d9bd2f346f16a0920028e1f0cc612",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3521a9fe1b055d3b32ea",
+    "url": "./static/css/2.5785371e.chunk.css"
+  },
+  {
+    "revision": "0379891ed781df2462c4",
+    "url": "./static/css/main.9481384e.chunk.css"
+  },
+  {
+    "revision": "3521a9fe1b055d3b32ea",
+    "url": "./static/js/2.8c3b45ea.chunk.js"
+  },
+  {
+    "revision": "6c6c336a3932c8bb21aaaf70b4848653",
+    "url": "./static/js/2.8c3b45ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0379891ed781df2462c4",
+    "url": "./static/js/main.b1cbdd3a.chunk.js"
+  },
+  {
+    "revision": "8979e4582ac24e1154b7",
+    "url": "./static/js/runtime-main.89205488.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "./static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "./static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "./static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);

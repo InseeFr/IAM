71dde2c4bb57ec69d9c19aa837892e7594f4c32c
NicoLaval <nicolas.laval@insee.fr>
2020-11-30 12:10:54 +0100

    Fix RolesPicker

--- src/roles-picker/index.js
+++ src/roles-picker/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './roles-picker.scss';
 import D from '../build-dictionary';
 
@@ -32,94 +32,81 @@ export default function RolesPicker({
 	const wrapperRef = useRef(null);
 	useOutsideAlerter(wrapperRef, () => setOpen(false));
 
-	const handleClick = useCallback(
-		e => {
-			e.preventDefault();
-			setOpen(!open);
-		},
-		[open]
-	);
+	const handleButtonClick = () => {
+		const addArray = userRoles.filter(role => !person.roles.includes(role));
+		const delArray = person.roles.filter(role => !userRoles.includes(role));
 
-	const handleButtonClick = useCallback(
-		e => {
-			const addArray = userRoles.filter(role => !person.roles.includes(role));
-			const delArray = person.roles.filter(role => !userRoles.includes(role));
-
-			const toAdd =
-				addArray.length > 0
-					? [
-							{
-								id: person.id,
-								roles: addArray.map(
-									label => roles.find(r => r.label === label).id
-								),
-							},
-					  ]
-					: [];
-
-			const toDelete =
-				delArray.length > 0
-					? delArray.map(label => ({
-							role: roles.find(r => r.label === label).id,
+		const toAdd =
+			addArray.length > 0
+				? [
+						{
 							id: person.id,
-					  }))
-					: [];
+							roles: addArray.map(
+								label => roles.find(r => r.label === label).id
+							),
+						},
+				  ]
+				: [];
 
-			const updates = {
-				toAdd,
-				toDelete,
-			};
-			handleSubmit(updates);
+		const toDelete =
+			delArray.length > 0
+				? delArray.map(label => ({
+						role: roles.find(r => r.label === label).id,
+						id: person.id,
+				  }))
+				: [];
 
-			setOpen(!open);
-		},
-		[userRoles]
-	);
+		const updates = {
+			toAdd,
+			toDelete,
+		};
+		handleSubmit(updates);
 
-	const handleRoleClick = role =>
-		useCallback(
-			e => {
-				const newUserRoles = userRoles.includes(role.label)
-					? userRoles.filter(label => label !== role.label)
-					: [...userRoles, role.label];
+		setOpen(!open);
+	};
 
-				setUserRoles(newUserRoles);
-			},
-			[userRoles]
-		);
+	const handleRoleClick = role => {
+		const newUserRoles = userRoles.includes(role.label)
+			? userRoles.filter(label => label !== role.label)
+			: [...userRoles, role.label];
+
+		setUserRoles(newUserRoles);
+	};
+
 	return (
-		<div
-			ref={wrapperRef}
-			className={`iam-roles-picker btn-group ${open ? 'open' : ''}`}
-		>
+		<div ref={wrapperRef} className={`iam-roles-picker btn-group`}>
 			<button
 				type="button"
 				className="btn btn-default dropdown-toggle"
 				data-toggle="dropdown"
 				aria-haspopup="true"
 				aria-expanded="false"
-				onClick={handleClick}
+				onClick={() => {
+					setOpen(!open);
+				}}
 			>
 				{D.manage} <span className="caret"></span>
 			</button>
-			<ul className="dropdown-menu">
-				{roles.map(role => (
-					<li key={role.label}>
-						<label>
-							<input
-								onChange={handleRoleClick(role)}
-								type="checkbox"
-								checked={userRoles.indexOf(role.label) >= 0}
-							/>
-							{role.label}
-						</label>
-					</li>
-				))}
+			{open && (
+				<ul className="dropdown-menu show">
+					{roles.map(role => (
+						<li key={role.label}>
+							<label>
+								<input
+									onChange={() => handleRoleClick(role)}
+									type="checkbox"
+									checked={userRoles.indexOf(role.label) >= 0}
+								/>
+								{role.label}
+							</label>
+						</li>
+					))}
 
-				<li>
-					<button onClick={handleButtonClick}>{D.save}</button>
-				</li>
-			</ul>
+					<li>
+						<button onClick={handleButtonClick}>{D.save}</button>
+					</li>
+				</ul>
+			)}
 		</div>
 	);
 }

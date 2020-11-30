22756a03d7846acb02b836645524775046812663
Nicolas Laval <nico.laval28@gmail.com>
2020-11-30 14:15:12 +0100

    Upgrade deps (#15)
    * Upgrade deps
    
    * Fix RolesPicker
    
    * Improve RolesPicker tests
    
    * Remove example
    
    * Update deploy.sh

--- example/README.md
+++
@@ -1,3 +0,0 @@
-# IAM example
-
-A simple example, showing how IAM can be used, is deployed [online](https://inseefr.github.io/IAM/example).
--- example/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app';
-import 'bootstrap/dist/css/bootstrap.css';
-import '@inseefr/wilco/dist/index.css';
-import '@inseefr/iam/dist/index.css';
-import './index.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
